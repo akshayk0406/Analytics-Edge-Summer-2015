3b34c04e865f6596251c1c6cba02d4391f531fde
Akshay Kulkarni <akshaykulkarni@Akshays-MacBook-Pro.local>
2015-09-19 00:49:08 -0500

    [Clustering] hierarchical clustering

---
+++ Clustering/ImageSegmentation.R
@@ -0,0 +1,20 @@
+setwd("/Users/akshaykulkarni/RProjects/analyticsedge")
+download.file("https://courses.edx.org/asset-v1:MITx+15.071x_2a+2T2015+type@asset+block/flower.csv",destfile="data/flower.csv",method="curl")
+#download.file("https://courses.edx.org/asset-v1:MITx+15.071x_2a+2T2015+type@asset+block/healthy.csv",destfile="data/healthy.csv",method="curl")
+#download.file("https://courses.edx.org/asset-v1:MITx+15.071x_2a+2T2015+type@asset+block/tumor.csv",destfile="data/tumor.csv",method="curl")
+
+flower = read.csv("data/flower.csv",header=FALSE) #Pixel Intensity Matrix
+flowerMatrix = as.matrix(flower) #This is crucial step. First convert input data-frame to matrix. Then convert the resulting matrix to vector
+flowerVector = as.vector(flowerMatrix)
+distance = dist(flowerVector,method="euclidean")
+
+clusterIntensity = hclust(distance,method="ward.D") #Minimize variance within each cluster and distance among cluster
+plot(clusterIntensity)
+rect.hclust(clusterIntensity,k=3,border="red")
+flowerClusters = cutree(clusterIntensity,k=3)
+tapply(flowerVector,flowerClusters,mean)
+dim(flowerClusters) = c(50,50)
+image(flowerClusters,axes=FALSE)
+image(flowerMatrix,axes=FALSE,col = grey(seq(0,1,length=256)))
+#healthy = read.csv("data/healthy.csv")
+#tumor = read.csv("data/tumor.csv")
